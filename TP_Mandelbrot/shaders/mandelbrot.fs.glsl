652ca6096c91cfdea47767e9a933b2bc48365441
Elia <eliaroard@gmail.com>
2023-03-07 13:43:51 +0100

    mandelbrot

--- TP_Mandelbrot/shaders/mandelbrot.fs.glsl
+++ TP_Mandelbrot/shaders/mandelbrot.fs.glsl
@@ -1,11 +1,27 @@
-#version 330
+#version 330 core
 
-in vec3 vColor;
+in vec3 vFragColor;
 in vec2 vFragPosition;
-
 
 out vec3 fFragColor;
 
-void main() {
-    fFragColor = vColor;
+vec2 complexSqr(vec2 z){
+	return vec2(z.x*z.x - z.y*z.y, 2*z.x*z.y);
 }
+
+void main() {
+  	//fFragColor =  vec3(vFragPosition.x, vFragPosition.y, 0.);	
+
+	vec2 z = vFragPosition;
+
+	int n_max = 30;
+	int i;
+	for(i=0; i<n_max && length(z)<2; i++){
+		z = complexSqr(z)+vFragPosition;
+	}
+
+	fFragColor= vec3(i/float(n_max), 0., 0.);
+
+	//fFragColor =  vec3(1., 0., 0.);
+	//fFragColor = vFragColor;
+};
--- TP_Mandelbrot/shaders/mandelbrot.vs.glsl
+++ TP_Mandelbrot/shaders/mandelbrot.vs.glsl
@@ -1,12 +1,13 @@
 #version 330
 
 layout(location = 0) in vec2 aVertexPosition;
-//layout(location = 8) in vec3 aVertexColor;
+//layout(location = 1) in vec3 aVertexColor;
 
 out vec2 vFragPosition; // frag position
 out vec3 vColor;
 
 void main() {
-    vColor = vec3(1.0, 0.0, 0.0);
+    vColor = vec3(1.,1.,1.);
+    vFragPosition = aVertexPosition;
     gl_Position = vec4(aVertexPosition, 0, 1);
 }
