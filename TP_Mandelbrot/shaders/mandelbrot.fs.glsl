08c27b30f373316bf86071912b77735f59188051
Elia <eliaroard@gmail.com>
2023-03-09 16:58:50 +0100

    mandelbrot2

--- TP_Mandelbrot/shaders/mandelbrot.fs.glsl
+++ TP_Mandelbrot/shaders/mandelbrot.fs.glsl
@@ -5,23 +5,18 @@ in vec2 vFragPosition;
 
 out vec3 fFragColor;
 
-vec2 complexSqr(vec2 z){
+vec2 ComplexSqr(vec2 z){
 	return vec2(z.x*z.x - z.y*z.y, 2*z.x*z.y);
 }
 
 void main() {
-  	//fFragColor =  vec3(vFragPosition.x, vFragPosition.y, 0.);	
-
 	vec2 z = vFragPosition;
 
-	int n_max = 30;
+	int N = 30;
 	int i;
-	for(i=0; i<n_max && length(z)<2; i++){
-		z = complexSqr(z)+vFragPosition;
+	for(i = 0; i < N && length(z)<2; i++){
+		z = ComplexSqr(z)+vFragPosition;
 	}
 
-	fFragColor= vec3(i/float(n_max), 0., 0.);
-
-	//fFragColor =  vec3(1., 0., 0.);
-	//fFragColor = vFragColor;
+	fFragColor= vec3(i/float(N), 0., 0.);
 };
