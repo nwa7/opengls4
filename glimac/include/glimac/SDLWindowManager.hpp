ebbfa9f899d7de2cd2a87b364d5cb42ef7f225a7
Elia <eliaroard@gmail.com>
2023-04-11 09:20:51 +0200

    cute first moon

--- glimac/include/glimac/SDLWindowManager.hpp
+++ glimac/include/glimac/SDLWindowManager.hpp
@@ -1,30 +1,30 @@
 #pragma once
 
-#include <cstdint>
-#include <SDL/SDL.h>
 #include "glm.hpp"
+#include <SDL/SDL.h>
+#include <cstdint>
 
 namespace glimac {
 
 class SDLWindowManager {
 public:
-    SDLWindowManager(uint32_t width, uint32_t height, const char* title);
+  SDLWindowManager(uint32_t width, uint32_t height, const char *title);
 
-    ~SDLWindowManager();
+  ~SDLWindowManager();
 
-    bool pollEvent(SDL_Event& e);
+  bool pollEvent(SDL_Event &e);
 
-    bool isKeyPressed(SDLKey key) const;
+  bool isKeyPressed(SDLKey key) const;
 
-    // button can SDL_BUTTON_LEFT, SDL_BUTTON_RIGHT and SDL_BUTTON_MIDDLE
-    bool isMouseButtonPressed(uint32_t button) const;
+  // button can SDL_BUTTON_LEFT, SDL_BUTTON_RIGHT and SDL_BUTTON_MIDDLE
+  bool isMouseButtonPressed(uint32_t button) const;
 
-    glm::ivec2 getMousePosition() const;
+  glm::ivec2 getMousePosition() const;
 
-    void swapBuffers();
+  void swapBuffers();
 
-    // Return the time in seconds
-    float getTime() const;
+  // Return the time in seconds
+  float getTime() const;
 };
 
-}
+} // namespace glimac
